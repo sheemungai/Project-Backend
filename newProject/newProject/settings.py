4101f6542eae3feb1863ae83e5dc28b1cb92b6cc
sheemungai <mungaisheilla19@gmail.com>
2026-02-07 21:23:08 +0300

    adjusted the settings

--- newProject/newProject/settings.py
+++ newProject/newProject/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'assessment',
-    
-
 ]
 
 MIDDLEWARE = [
@@ -77,15 +75,11 @@ WSGI_APPLICATION = 'newProject.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'university',
-            'USER': 'postgres',
-            'PASSWORD': '12345',
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 
 # Password validation
