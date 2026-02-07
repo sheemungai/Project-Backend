02348c28c54bb0e9d3368e547fff505abc2508b9
sheemungai <mungaisheilla19@gmail.com>
2026-02-07 13:07:51 +0300

    added student grade and preferences

--- newProject/students/urls.py
+++ newProject/students/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import MyProfileView, ProfileViewset
+from .views import MyProfileView, ProfileViewset, StudentGradesViewSet, StudentPreferencesView
 
 
 urlpatterns = [
     path('profiles/', ProfileViewset.as_view()),
     path('profiles/<int:id>/', ProfileViewset.as_view()),
     path('myprofile/', MyProfileView.as_view()),
+
+
+    path('grades/', StudentGradesViewSet.as_view(), name='grades-list'),
+    path('grades/<int:id>/', StudentGradesViewSet.as_view(), name='grades-detail'),
+    
+    # Preferences endpoint
+    path('preferences/', StudentPreferencesView.as_view(), name='preferences'),
 ]
